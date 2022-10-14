6f1dfcfb20026adfe6e6ccfcd835e352da1b4249
avp02 <67960868+avp02@users.noreply.github.com>
2022-10-14 17:09:32 +0300

    Add files via upload

---
+++ src/test/java/com/liquebase/springbootliquibase/SpringBootLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquebase.springbootliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
